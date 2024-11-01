4e52237bfa29de1c31d2d7649159f450772fa0ef
agorapilled <johngardner13@outlook.com>
2024-11-01 17:26:41 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const [fallingEmojis, setFallingEmojis] = useState([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const totalImages = 10;
+  const totalImages = 11;
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(null);
   const videoRef = useRef(null);
